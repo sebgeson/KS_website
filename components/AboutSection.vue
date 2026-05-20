344b189bd52c3e4ab2a4ae1eaebdab09c70929f3
sebgeson <sebbelol95@gmail.com>
2026-05-20 13:01:41 +0200

    m

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -5,9 +5,7 @@
         <p class="section__eyebrow">Om oss</p>
         <h2 id="about-title" class="section__title">En ansvarlig partner for tekniske brannsikringsprosjekter.</h2>
         <p class="section__lead">
-          Komplett Sprinkler AS utfør sprinkleranlegg der detaljene betyr noe:
-          riktig dimensjonering, ryddig installasjon, tydelig dokumentasjon og stabil service etter
-          overlevering.
+          Komplett Sprinkler AS utfør sprinkleranlegg der detaljene betyr noe: Fra gjennomtenkt prosjektering, ryddig installasjon, tydelig dokumentasjon og til ferdigstilling av prosjektet.
         </p>
         <p>
           Vi kombinerer praktisk fagkunnskap med strukturert prosjektstyring. Det gjør oss til
